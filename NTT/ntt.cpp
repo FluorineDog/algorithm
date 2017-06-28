a912049cf43a502c764878ba53b068ba872a88ad
fluorinedog <fluorinedog@gmail.com>
2017-06-28 12:36:58 +0800

    add meta-programming

--- NTT/ntt.cpp
+++ NTT/ntt.cpp
@@ -1,23 +1,8 @@
 #include "ntt.h"
+#include "ntt_helper.h"
 #include <cmath>
 using std::vector;
 #include <unordered_map>
-constexpr int truck_sz = 4;  // vary with N
-constexpr int n_sz = 4;     // maximum 20 with truck_sz = 6
-
-constexpr ull N = 1 << n_sz;        
-constexpr ull truck = 1 << truck_sz; 
-
-static_assert(n_sz + 2 * truck_sz < 32, "truck limit exceeds");
-constexpr ull Prime = 3*(1<<12) + 1;      // must be a prime
-constexpr ull Root = 4134;                // must be primitive root^k
-constexpr ull RootRev = 10984;            // must set accordingly
-constexpr ull NRev  = 11521;              // must set accordingly
-static_assert(Prime < (1ull << 32), "prime is too large");
-static_assert(Prime % N == 1 ,  "prime should be the form of k * N + 1");
-static_assert((N * NRev) % Prime == 1, "N^-1 != NRev");
-static_assert((Root * RootRev) % Prime == 1,  "Root^-1 != RootRev");
-
 TimeDomain init(const std::vector<unsigned int>& number){
   constexpr int input_base_sz = 32;
   // assume base = 1 << 32
--- NTT/ntt_helper.h
+++ NTT/ntt_helper.h
@@ -1,16 +1,41 @@
+#ifndef DOG_NTT_HELPER_H_
+#define DOG_NTT_HELPER_H_
+#include "ntt.h"
+constexpr int truck_sz = 4;  // vary with N
+constexpr int n_sz = 4;     // maximum 20 with truck_sz = 6
 
-using ull = unsigned int;
+constexpr ull N = 1 << n_sz;        
+constexpr ull truck = 1 << truck_sz; 
 
+static_assert(n_sz + 2 * truck_sz < 32, "truck limit exceeds");
+constexpr ull Prime = 3*(1<<12) + 1;      // must be a prime
+constexpr ull PrimitiveRoot = 11;         // must set accordingly
+static_assert(Prime < (1ull << 32), "prime is too large");
+static_assert(Prime % N == 1 ,  "prime should be the form of k * N + 1");
 
 template <int exp>
-struct PowULL{
+struct Helper{
+  static constexpr int half = exp / 2;
+  static constexpr int keep = exp & 1;
   static constexpr ull pow(const ull base)
   {
-    return 0;
+    // return Helper<bas::;
+    const ull rec = Helper<half>::pow(base);
+    return rec*rec*(keep?base:1) % Prime;
   }
 };
 
 template <>
-struct PowULL<1>{
+struct Helper<1>{
   static constexpr ull pow(const ull base){return base;}
 };
+
+
+constexpr ull Kappa = (Prime - 1) / N;
+constexpr ull Root = Helper<Kappa>::pow(PrimitiveRoot);                
+constexpr ull RootRev = Helper<Prime-2>::pow(Root);                 
+constexpr ull NRev  = Helper<Prime-2>::pow(N);                      
+static_assert((N * NRev) % Prime == 1, "N^-1 != NRev");
+static_assert((Root * RootRev) % Prime == 1,  "Root^-1 != RootRev");
+
+#endif

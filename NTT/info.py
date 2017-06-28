13d2d150310befba37b5e7c55d49b24629edf01f
fluorinedog <fluorinedog@gmail.com>
2017-06-29 01:48:44 +0800

    support file

--- NTT/info.py
+++ NTT/info.py
@@ -1,10 +1,30 @@
 #!/bin/python
+from gmpy import *
+
+def powp(n, exp, p):
+  if(exp == 0):
+    return 1
+  tmp = powp(n, exp//2, p)
+  if exp%2 == 0:
+    return tmp * tmp % p
+  else:
+    return tmp * tmp * n % p
+
 def main():
-  p = 3*2**12 + 1
-  print("pp=", p) 
-  root = 4134
-  rootRev = root ** (p-2) % p
-  arr = [root**i%p for i in range(0,16)]
-  print(root, rootRev)
-  print(arr)
+  n = 2**20
+  mk = 2**8
+  prime_arr = [ (i,p) for (i, p) in [(i, i*n+1) for i in range(mk+0,mk+20)] if is_prime(p) ]
+  k, p = prime_arr[0]
+  print(prime_arr)
+  root_arr = [root for root in [i**k % p for i in range(2,22)] if powp(root,n//2,p) == p-1]
+  root = root_arr[0]
+  rootRev = powp(root, p-2, p)
+  NRev = powp(n, p-2, p)
+  print(root, rootRev, p)
+  print(n, NRev)
+  # rootRev = root ** (p-2) % p
+  # arr = [root**i%p for i in range(0,16)]
+  # print(root, rootRev)
+  # print(arr)
 main()
+

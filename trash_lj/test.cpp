3f8a4b535a8ee6237ea5e8ea3c56aaf770b53bfb
FluorineDog <fluorine@gmail.com>
2017-11-07 16:30:17 +0800

    finish_multipack

--- trash_lj/test.cpp
+++ trash_lj/test.cpp
@@ -1,1 +1,24 @@
 #include "trash.h"
+#include <gtest/gtest.h>
+TEST(trash, case1){
+	vector<tuple<size_t, double, size_t>> data {
+		{1, 1, 3},
+		{1, 2, 3},
+		{1, 3, 3},
+		{1, 4, 3},
+		{1, 5, 3},
+		{1, 6, 3},
+	};
+	EXPECT_EQ(6, multipack(data, 1)); 
+	EXPECT_EQ(12, multipack(data, 2)); 
+	EXPECT_EQ(18, multipack(data, 3)); 
+	EXPECT_EQ(18 + 5, multipack(data, 4)); 
+	EXPECT_EQ(18 + 10, multipack(data, 5)); 
+	EXPECT_EQ(18 + 15, multipack(data, 6)); 
+	EXPECT_EQ(18 + 15 + 4, multipack(data, 7)); 
+	EXPECT_EQ(18 + 15 + 8, multipack(data, 8)); 
+	EXPECT_EQ(18 + 15 + 12, multipack(data, 9)); 
+	EXPECT_EQ(18 + 15 + 12 + 3, multipack(data, 10)); 
+	EXPECT_EQ(18 + 15 + 12 + 6, multipack(data, 11)); 
+	EXPECT_EQ(18 + 15 + 12 + 9, multipack(data, 12)); 
+}
--- trash_lj/trash.h
+++ trash_lj/trash.h
@@ -6,28 +6,34 @@ using namespace std;
 
 double multipack(vector<tuple<size_t, double, size_t>> data, size_t limit) {
   // define limit * (status)
-  // define status = index(pack_type) *
+  // define status = weight(pack_type) *
   vector<double> slot(limit + 1, 0);
-  for (auto[weight, value, count] : data) {
-#pragma omp parallel for
-    for (int offset = 0; offset < weight; ++offset) {
-      deque<pair<int, int>> q;
-      for (int index = offset; index < limit + 1; index += weight) {
-        if (!q.empty() && q.front().first == index + count * weight) {
+  for (auto[new_weight, new_value, new_count] : data) {
+    // #pragma omp parallel for
+    new_count += 1;
+    for (size_t offset = 0; offset < new_weight; ++offset) {
+      deque<pair<int, double>> q;
+      // for (size_t weight = offset; weight < limit + 1; weight += new_weight)
+      // {
+      for (size_t index = 0; index < (limit + 1 - offset) / new_weight;
+           index++) {
+        auto weight = offset + index * new_weight;
+        if (!q.empty() && q.front().first == index - new_count) {
           q.pop_front();
         }
         while (!q.empty()) {
-          auto[last_w, last_v] = q.back();
-          auto old_value = (index - last_w) / weight + last_v;
-          if (old_value > slot[index])
+          auto[last_i, last_v] = q.back();
+          auto old_value = (index - last_i) * new_value + last_v;
+          if (old_value > slot[weight])
             break;
           q.pop_back();
         }
-        q.push_back(make_pair(index, value));
-        auto[w, v] = q.back();
-        auto max_value = (index - w) / weight + v;
+        q.push_back(make_pair(index, slot[weight]));
+        auto[best_i, best_v] = q.front();
+        auto max_value = (index - best_i) * new_value + best_v;
         slot[index] = max_value;
       }
     }
   }
+  return slot[limit];
 }
